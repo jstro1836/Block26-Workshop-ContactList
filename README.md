467228e953fb5dc687c9606a5aae4b81580b7072
jstro1836 <39538412+jstro1836@users.noreply.github.com>
2024-03-27 16:06:00 -0500

    Resubmitting the workshop.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Block 26 Workshop
+
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import ContactList from './ContactList';
+import './App.css'
+
+function App() {
+  const [contactList, setContactList] = useState([]);
+
+  const BASE_URL = 'https://jsonplaceholder.typicode.com/users';
+
+  useEffect(() => {
+    const getContacts = async() => {
+      const response = await fetch(`${BASE_URL}`);
+      const allContacts= await response.json();
+      setContactList(allContacts);
+    }
+
+    getContacts();
+  }, []);
+
+  return (
+    <>
+      <h1>Contact List</h1>
+      <ContactList contactList={contactList} />
+    </>
+  )
+}
+
+export default App
