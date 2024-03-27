9e4dd0fec7e812c52a545515b692e9711005b663
jstro1836 <39538412+jstro1836@users.noreply.github.com>
2024-03-27 17:51:19 -0500

    Updated contact list within functional component.

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -1,4 +1,4 @@
-const ContactList = ({ contactList }) => { // <-- Destructure the prop here
+const ContactList = ({ contactList }) => { // <-- Destructuring the prop here
   return (
     <div className="contact-list-container">
       <h2>Contact List</h2>
@@ -8,7 +8,7 @@ const ContactList = ({ contactList }) => { // <-- Destructure the prop here
           <div className="header-item">Email</div>
           <div className="header-item">Phone</div>
         </div>
-        {contactList.map((contact, index) => ( // <-- Use the parameter `contact` here
+        {contactList.map((contact, index) => ( // <-- Using the parameter `contact` here
           <div className="contact-item" key={index}>
             <div className="item-name">{contact.name}</div>
             <div className="item-email">{contact.email}</div>
